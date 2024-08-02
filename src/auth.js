c534367bcfa84e8d4c6f91ed93901e7d88a4682c
Alexander Opyrchal <66843881+alexopyrchal8@users.noreply.github.com>
2024-08-02 18:35:37 -0400

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 const clientId = "16d807650a134e54ae7cf5830863ab50"; // Replace with your actual client ID
-const redirectUri = "https://alexopyrchal8.github.io/index.html"; // Update this with your actual redirect URI
+const redirectUri = "http://localhost:5173/callback"; // Update this with your actual redirect URI
 
 document.getElementById('login-button').addEventListener('click', () => {
     redirectToAuthCodeFlow(clientId);
